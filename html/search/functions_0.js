e2a7e5a3786255c09c54addb372659790f2b4b9a
Max <163636576+nishka322@users.noreply.github.com>
2024-10-27 14:36:03 +0300

    Doxygen add

---
+++ html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['canmoveto_0',['canMoveTo',['../class_screen_string.html#abe3e4df5e00016d0a60ab318bc4746d3',1,'ScreenString']]]
+];
---
+++ html/search/functions_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['_7escreenstring_0',['~ScreenString',['../class_screen_string.html#a8594549aea0f53bb5ff4c94680d2b42f',1,'ScreenString']]]
+];
