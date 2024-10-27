553b852c5c18948c4c475265406427c09f4ca6a9
Max <163636576+nishka322@users.noreply.github.com>
2024-10-27 14:47:40 +0300

    Add files via upload

---
+++ search/all_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['screenstring_0',['ScreenString',['../class_screen_string.html',1,'ScreenString'],['../class_screen_string.html#a002c6d8aea7155c7e0e1f5ad2cb7805b',1,'ScreenString::ScreenString()']]]
+];
---
+++ search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['screenstring_0',['ScreenString',['../class_screen_string.html',1,'']]]
+];
